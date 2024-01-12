21d86c200738a9147bf834b3844de4e4df9b193d
mahesh <mahesh2885858@gmail.com>
2024-01-12 16:35:33 +0530

    misc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "medium-purple": "#8884d8",
+        "medium-black": "#3f3f3f",
+      },
     },
   },
   plugins: [],
